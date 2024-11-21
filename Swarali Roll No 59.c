693cb86993d3cd975264c0c6615ce05788f486cd
Swarali16-max <swaralikarkar16@gmail.com>
2024-11-21 22:51:46 +0530

    Create Swarali Roll No 59.c

---
+++ Swarali Roll No 59.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    printf("Roll No.\tName of Student\t\tMarks\n");
+    printf("1\t\tJon Doe\t\t\t35\n");
+    printf("2\t\tRancho Chanchad\t\t37\n");
+    printf("3\t\tSanjay Singh\t\t56\n");
+    return 0;
+}
+
+
+Output
+Roll No.        Name of Student         Marks
+1               Jon Doe                 35
+2               Rancho Chanchad         37
+3               Sanjay Singh            56
+
+
+...Program finished with exit code 0
+Press ENTER to exit console.
